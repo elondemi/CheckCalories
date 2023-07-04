fc8e4657bcef1a6dc615c6be076d3cbb0256679a
Elon Demi <elon.demi@student.uni-pr.edu>
2023-07-04 22:24:12 +0200

    Initial commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by taulant on 2023-07-04.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calories
+//
+//  Created by taulant on 2023-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
